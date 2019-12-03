5a420df46d6686f05f5341207fac80f100d350e3
hndsmkkgr <phoebe_yes@live.com>
2019-12-03 22:15:05 +0700

    halaman platform udah rapih

--- application/views/platforms.php
+++ application/views/platforms.php
@@ -1,44 +1,47 @@
 
 
 <div class="home" style="margin-top: 50px">
-    <div class="home_container">
-      <div class="home_background" style="background-image:url(assets/images/platform.jpg)"></div>
-      <div class="home_content_container">
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              <div class="home_content">
-                <div class="home_title">Find Your Game<span>.</span></div>
-                <div class="home_text"><p>Get just the right game to satisfy your boredom.</p></div>
-              </div>
+  <div class="home_container">
+    <div class="home_background" style="background-image:url(assets/images/platform.jpg)"></div>
+    <div class="home_content_container">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="home_content">
+              <div class="home_title">Find Your Game<span>.</span></div>
+              <div class="home_text"><p>Get just the right game to satisfy your boredom.</p></div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 
 
   <div class="super_container">
 
-  <div class="container-fluid" style="margin-top: 50px" >
+	<div class="products">
+	  <div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="product_grid">  
 
-  <div class="row"style="margin-right: 10px; margin-left: 7rem;">
-    <?php foreach ($barang as $brg) : ?> 
-      <div class="product">
-							<div class="product_image"><img src="<?php echo base_url().'/upload/'.$brg->gambar?>" alt=""></div>
-							<div class="product_extra product_new"><a href="<?= base_url()?>detil/<?= $brg->id_brg;?>">New</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="<?= base_url()?>detil/<?= $brg->id_brg;?>"><?php echo $brg->nama_brg ?></a></div>
-								<div class="product_price">Rp. <?php echo $brg->harga; ?></div>
+            <?php foreach ($barang as $brg) : ?> 
+            <div class="product">
+                <div class="product_image"><img src="<?php echo base_url().'/upload/'.$brg->gambar?>" alt=""></div>
+                <div class="product_content">
+                <div class="product_title"><a href="<?= base_url()?>detil/<?= $brg->id_brg;?>"><?php echo $brg->nama_brg ?></a></div>
+                <div class="product_price">Rp. <?php echo $brg->harga; ?></div>
                 <br/>
                 <?php echo anchor ('platform/keranjang/'. $brg->id_brg, '<div class="btn btn-sm btn-primary">Add to Cart</div>') ?>
-
-							</div>
-						</div>
-  <?php endforeach; ?>
+              </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 </div>
 
 <div class="avds_xl">
