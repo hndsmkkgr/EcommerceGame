83b4dc8f2b878a6547bea286bd3fbb5b70e2913f
ariodwiponco <rio.dwiponco@gmail.com>
2019-12-03 11:46:49 +0700

    bikin controller cart sama bikin laman checkout

---
+++ controllers/cart.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contact extends CI_Controller {
+	public function __construct()
+		{
+			parent:: __construct();
+		}
+	public function index()
+	{
+		if($this->session->isLoggedin)
+		{
+			$this->load->view('header');
+		}
+		else
+		{
+			$this->load->view('header_guest');
+        }
+		$this->load->view('cart');
+		$this->load->view('footer');
+	}
+}
+?>
