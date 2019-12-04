0654d4be4b8e4f3f318ca4ed73bc002581ec9c60
ffaizin <fajarfaizin77@gmail.com>
2019-12-04 15:44:21 +0700

    memperbaiki tampilan detil

--- application/views/product.php
+++ application/views/product.php
@@ -1,24 +1,6 @@
-<div class="super_container">
+<div class="super_container"style="margin-top: 60px;">
 
-	<!-- Home -->
-
-	<div class="home">
-		<div class="home_container">
-			<div class="home_background" style="background-image:url(images/categories.jpg)"></div>
-			<div class="home_content_container">
-				<div class="container">
-					<div class="row">
-						<div class="col">
-							<div class="home_content">
-								<div class="home_title">Smart Phones<span>.</span></div>
-								<div class="home_text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a ultricies metus. Sed nec molestie eros. Sed viverra velit venenatis fermentum luctus.</p></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	
 
 	<!-- Product Details -->
 
@@ -38,7 +20,20 @@
 					<div class="details_content">
 						<div class="details_name"><?= $tampil_detil->nama_brg;?></div>
 						<div class="details_price">Rp. <?= $tampil_detil->harga;?></div>
-
+						<br>
+						<div class="details_price">Categori : <?= $tampil_detil->kategori;?></div>
+						<br>
+						<div class="details_price">Stock Product : <?= $tampil_detil->stok;?></div>
+						<br>
+						<br>
+						<?php
+						if($tampil_detil->stok > 0){
+							echo '<div class="">Product Available</div>';
+						}else{
+							echo '<div class="">Product Not available</div>';
+						}
+						?>
+						
 						<div class="details_text">
 							<p><?= $tampil_detil->detil;?></p>
 						</div>
@@ -54,7 +49,8 @@
 								<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 							</ul>
 						</div>
-
+							<br>
+							<br>
 						<div>
 							<?php echo anchor ('product/keranjang/'. $tampil_detil->id_brg, '<div class="btn btn-sm btn-primary">Add to Cart</div>') ?>
 						</div>
@@ -62,77 +58,6 @@
 				</div>
 			</div>
 
-			<div class="row description_row">
-				<div class="col">
-					<div class="description_title_container">
-						<div class="description_title">Description</div>
-						<div class="reviews_title"><a href="#">Reviews <span>(1)</span></a></div>
-					</div>
-					<div class="description_text">
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris consequat nisi ut mauris efficitur lacinia.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Products -->
-
-	<div class="products">
-		<div class="container">
-			<div class="row">
-				<div class="col text-center">
-					<div class="products_title">Related Products</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					
-					<div class="product_grid">
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_1.jpg" alt=""></div>
-							<div class="product_extra product_new"><a href="categories.html">New</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_2.jpg" alt=""></div>
-							<div class="product_extra product_sale"><a href="categories.html">Sale</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$520</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_3.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$710</div>
-							</div>
-						</div>
-
-						<!-- Product -->
-						<div class="product">
-							<div class="product_image"><img src="images/product_4.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$330</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 
 	<!-- Newsletter -->
 
@@ -159,32 +84,4 @@
 			</div>
 		</div>
 	</div>
-
-	<!-- Footer -->
-	
-	<div class="footer_overlay"></div>
-	<footer class="footer">
-		<div class="footer_background" style="background-image:url(images/footer.jpg)"></div>
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					<div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center">
-						<div class="footer_logo"><a href="#">Sublime.</a></div>
-						<div class="copyright ml-auto mr-auto"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-						<div class="footer_social ml-lg-auto">
-							<ul>
-								<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-								<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-								<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-								<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-</div>
 
