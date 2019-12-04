be94121749f21b15a9ef7d3b6060f5e13b6ea901
hndsmkkgr <phoebe_yes@live.com>
2019-12-04 18:12:08 +0700

    account udah bisa dibuka

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -5,12 +5,15 @@ class Account extends CI_Controller {
 	public function __construct()
 		{
 			parent:: __construct();
+			$this->load->model('user');
 		}
 	public function index()
 	{	
 		$this->load->view('header');
-		$this->load->view('account');
+		$this->load->view('account_personal');
 		$this->load->view('footer');
+
+		// $email = $this->input->post('email');
 	}
 }
 ?>
--- application/views/footer.php
+++ application/views/footer.php
@@ -24,30 +24,28 @@
 			</div>
 		</div>
     </footer>
-</body>
-</html>
 
-
-<script src="<? echo base_url();?>assets/js/jquery-3.2.1.min.js"></script>
-<script src="<? echo base_url();?>assets/styles/bootstrap4/popper.js"></script>
-<script src="<? echo base_url();?>assets/styles/bootstrap4/bootstrap.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/greensock/TweenMax.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/greensock/TimelineMax.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/scrollmagic/ScrollMagic.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/greensock/animation.gsap.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/greensock/ScrollToPlugin.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="<? echo base_url();?>assets/plugins/Isotope/isotope.pkgd.min.js"></script>
-<script src="<? echo base_url();?>assets/plugins/easing/easing.js"></script>
-<script src="<? echo base_url();?>assets/plugins/parallax-js-master/parallax.min.js"></script>
-<script src="<? echo base_url();?>assets/js/custom.js"></script>
+<script src="<?php base_url();?>assets/js/jquery-3.2.1.min.js"></script>
+<script src="<?php base_url();?>assets/styles/bootstrap4/popper.js"></script>
+<script src="<?php base_url();?>assets/styles/bootstrap4/bootstrap.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/greensock/TweenMax.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/greensock/TimelineMax.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/greensock/animation.gsap.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="<?php base_url();?>assets/plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="<?php base_url();?>assets/plugins/easing/easing.js"></script>
+<script src="<?php base_url();?>assets/plugins/parallax-js-master/parallax.min.js"></script>
+<script src="<?php base_url();?>assets/js/custom.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCIwF204lFZg1y4kPSIhKaHEXMLYxxuMhA"></script>
-<script src="<?php echo base_url();?>assets/js/contact.js"></script>
+<script src="<?php base_url();?>assets/js/contact.js"></script>
 
 <!-- subscribe -->
 <script>
-	function popup_home() {
-  	alert("Thank you for subscribing to our newsletter!");
+	function popup_home()
+	{
+		alert("Thank you for subscribing to our newsletter!");
 	}
 </script>
 
@@ -85,3 +83,99 @@
 		window.onload=function(){grayer('personal_form',true);}; // disabled by default
 	}
 </script>
+
+<script type="text/JavaScript">
+
+/* Edit and Save Profile*/
+
+function grayerProfile(profile_form, yesNo) 
+{
+	  var f = document.getElementById(profile_form);
+	   for(var i=0; i<f.length; i++) f[i].disabled = yesNo;
+
+	window.onload=function(){grayerProfile('profile_form',true);}; // disabled by default
+}
+
+/* Edit and Save */
+
+function grayer(personal_form, yesNo) 
+{
+	  var f = document.getElementById(personal_form);
+	   for(var i=0; i<f.length; i++) f[i].disabled = yesNo;
+
+	window.onload=function(){grayer('personal_form',true);}; // disabled by default
+}
+
+/* Edit and Save Password*/
+
+function grayer(security_form, yesNo) 
+{
+	  var f = document.getElementById(security_form);
+	   for(var i=0; i<f.length; i++) f[i].disabled = yesNo;
+
+	window.onload=function(){grayer('security_form',true);}; // disabled by default
+}
+
+/*-----------------------------------------------------------------------------------------------*/
+
+/* Display Profile Button */
+
+function changeVisibilityProfile() {
+	  document.getElementById("profile_save").style.display = "none";
+}
+
+function changeVisibilityProfile2() {
+	document.getElementById("profile_save").style.display = "block";
+}
+
+/* Display Security Settings Button */
+
+	function changeVisibilityPassword() {
+	  document.getElementById("security_save").style.display = "none";
+}
+
+function changeVisibilityPassword2() {
+	document.getElementById("security_save").style.display = "block";
+}
+
+
+/* Display Button */
+
+function changeVisibility() {
+	  document.getElementById("save").style.display = "none";
+}
+
+function changeVisibility2() {
+	document.getElementById("save").style.display = "block";
+}
+
+/* Display Security Settings */
+
+function changeVisibilitySecurity() {
+	  document.getElementById("security").style.display = "none";
+}
+
+function changeVisibilitySecurity2() {
+	document.getElementById("security").style.display = "block";
+}
+
+/* Display Security Personal Information */
+
+function changeVisibilityPersonal() {
+	  document.getElementById("personal_information").style.display = "none";
+}
+
+function changeVisibilityPersonal2() {
+	document.getElementById("personal_information").style.display = "block";
+}
+
+/*-----------------------------------------------------------------------------------------------*/
+
+</script>
+</div>
+
+<!-- <script src="<?php echo base_url('assets/js/checkout.js')?>" type="text/JavaScript"></script> -->
+
+
+</body>
+</html>
